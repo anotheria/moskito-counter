505cb06993f76e4e434c5be860578b4b590b42b2
Leon Rosenberg <leon.rosenberg@anotheria.net>
2013-12-02 10:56:38 +0100

    finalizing counter, adding versions

---
+++ java/net/anotheria/moskitocounter/VersionCounter.java
@@ -0,0 +1,19 @@
+package net.anotheria.moskitocounter;
+
+import net.anotheria.moskito.aop.annotation.CountByParameter;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 29.11.13 17:57
+ */
+public class VersionCounter {
+	@CountByParameter public void control(String version){
+
+	}
+
+	@CountByParameter public void webui(String version){
+
+	}
+}
